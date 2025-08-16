7737dedc7879a9a4cc4cef3700ecf56fd8bd6c9a
LuisAAA <luisalvarealmeida@gmail.com>
2025-08-15 21:42:45 -0500

    chore: update Drupal core and improve .gitignore for database backups

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -750,7 +750,11 @@ $settings['container_yamls'][] = $app_root . '/' . $site_path . '/services.yml';
  *
  * @see https://www.drupal.org/docs/installing-drupal/trusted-host-settings
  */
-# $settings['trusted_host_patterns'] = [];
+ $settings['trusted_host_patterns'] = [
+  '^localhost$',
+  '^127\.0\.0\.1$',
+  '^.+\.lndo\.site$', // dominios que genera Lando
+];
 
 /**
  * The default list of directories that will be ignored by Drupal's file API.
